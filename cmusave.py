d55428b875eb39b48a5dcef92fe5afadd700e9e6
Thomas Turney <aperture321@gmail.com>
2013-10-27 19:24:30 -0400

    Added streaming for somafm icecast metadata file.

---
+++ cmusave.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import urllib2
+import os
+
+#v1.0
+stream_url = 'http://ice.somafm.com/groovesalad'
+request = urllib2.Request(stream_url)
+try:
+    request.add_header('Icy-MetaData', 1)
+    response = urllib2.urlopen(request)
+    icy_metaint_header = response.headers.get('icy-metaint')
+    if icy_metaint_header is not None:
+        metaint = int(icy_metaint_header)
+        read_buffer = metaint+255
+        content = response.read(read_buffer)
+        title = content[metaint:].split("'")[1] + "\n"
+        os.chdir(os.path.expanduser("~"))
+        filew = open(".savedata.txt", "a+r")
+        infile = False
+        for line in filew:
+            if (line == title):
+                infile = True
+        if (not infile):
+            filew.write(title)
+        filew.close()
+except:
+    print 'Error'
